c75d65376c208ab8e6a250fdee3ad1581daabc0b
TrendyMateus <itzmaatzz@hotmail.com>
2021-11-11 21:08:06 -0300

    Create testeMenines.java
    Desafio da classe

---
+++ testeMenines.java
@@ -0,0 +1,19 @@
+
+// @author Mateus Brito
+
+
+
+public class testeMenines {
+
+    public static void main(String[] args) throws InterruptedException {
+
+    testeGravida();
+
+    }
+
+    public static void testeGravida() throws InterruptedException {
+
+        ProjetoFinal.menines();
+
+    }
+}
