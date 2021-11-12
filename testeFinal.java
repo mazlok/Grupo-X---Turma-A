49d4baef08e7857eed55639390a658279dc752c4
Nerdeiraria <lucas.958@hotmail.com>
2021-11-11 21:12:03 -0300

    Teste Final

---
+++ testeFinal.java
@@ -0,0 +1,16 @@
+// @author Lucas 
+
+public class testeFinal {
+
+    public static void main(String[] args) throws InterruptedException {
+
+    testeDesafioFinal();
+
+    }
+
+    public static void testeDesafioFinal() throws InterruptedException {
+
+        ProjetoFinal.desafioFinal();
+
+    }
+}
